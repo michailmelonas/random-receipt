04f85735702d8af2f9e8066621acceeaaf0c2a68
Michail Melonas <michail@rampgrowth.com>
2022-07-07 10:52:02 +0200

    Add image url

--- tests/test_receipt.py
+++ tests/test_receipt.py
@@ -5,8 +5,8 @@ from random_receipt.receipt import generate
 
 def test_receipt_contains_required_fields():
     receipt = generate()
-    required_fields = ["total", "lineItems", "tax", "storeName", "logoUrl", "imageUrl"]
-    assert all([k in required_fields for k in receipt.keys()])
+    required_fields = ["total", "lineItems", "tax", "storeName", "logoUrl"]
+    assert all([k in receipt.keys() for k in required_fields])
 
 
 def test_item_totals_match_grand_total():
